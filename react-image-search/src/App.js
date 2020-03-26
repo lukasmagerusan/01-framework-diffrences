21bbc2fcdbd9c0c26fe984d775c7e48279722258
Lukas Magerusan <lukasmagerusan@outlook.com>
2020-03-26 21:43:36 +0100

    add functionality

--- react-image-search/src/App.js
+++ react-image-search/src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [title, setTitle] = useState('🍨 JavaScript')
+  const [userInput, setUserInput] = useState('')
+  const [images, setImages] = useState([])
+  const [loadingImage, setLoadingImage] = useState(false)
+  const [noImageFound, setNoImageFound] = useState('')
+
+  const verification = value => {
+    value.length === 0 ? setNoImageFound('Kein Bilder wurde unter der Bezeichnung ' + '"' + userInput + '"' + ' gefunden!') : setNoImageFound('')
+  }
+
+  const loading = value => {
+    setLoadingImage(value)
+  }
+
+  const getImages = value => {
+    return fetch(`https://api.unsplash.com/search/photos?page=1&query=${value}&client_id=84sHIaQrR9rSmxJKVr4c1O2orNTm00OeANCnpZ-Yta0`)
+    .then(response => response.json())
+    .then(result => setImages(result.results) & verification(result.results))
+  }
+
+  const search = event => {
+    event.preventDefault();
+    loading(true)
+    getImages(userInput).then(() => loading(false))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{title}</h1>
+      <form onSubmit={search}>
+        <label htmlFor="searchTerm">Suche nach einem Bild</label>
+        <input
+          value={userInput}
+          className="u-full-width"
+          type="text"
+          onChange={e => setUserInput(e.target.value)}
+          />
+        <button type="submit">Suche</button>
+        </form>
+        <h1>{noImageFound}</h1>
+        { loadingImage ? <img className="loadingImage" alt="Loading..." src="https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"/> : ''}
+        <section className="imageSection">
+        {
+          images.map(image => {
+            return <img className="image" src={image.urls.regular} alt={image.description} key={image.id}/>
+          })
+        }
+        </section>
     </div>
   );
 }
